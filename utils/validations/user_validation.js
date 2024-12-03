c4cc15d5e11f605d6e952e5fb33b0dafffd80b74
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2024-12-03 09:21:13 -0800

    done

--- utils/validations/user_validation.js
+++ utils/validations/user_validation.js
@@ -4,6 +4,7 @@ export const regiterValidationSchema = Joi.object({
   userName: Joi.string().min(3).max(30).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).max(100).required(),
+  role:Joi.string()
   // phoneNumber: Joi.string().min(11).max(11).required(),
   // postCode: Joi.string().min(2).max(10).required(),
   // address: Joi.string().min(5).max(255).required(),
