58b6bafaf606efe0f831cf97dd43937728c61504
samiullahqureshi1 <samiullahqureshi669@gmail.com>
2025-01-04 03:52:07 -0800

    done

--- db/db.js
+++ db/db.js
@@ -4,9 +4,12 @@ const db_name = `Cosmetic`;
 export const db_connection = async () => {
     try {
         const db_instance = await mongoose.connect(`${process.env.MONGODB_URI}${db_name}`, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        })
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000, // 30 seconds
+            socketTimeoutMS: 45000, // 45 seconds
+        });
+        
         console.log(`Data Base Connected: ${db_instance.connection.host}`)
     } catch (error) {
         console.log(`Error Occurs:`, error.message)
